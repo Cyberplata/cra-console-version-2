c79ace43d4378ab506dd820c66f121e6fa50597c
PlatonKynin <kininplaton@gmail.com>
2023-12-03 21:05:20 +0300

    Add 33

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,10 @@ function App() {
         <div>developer/popup branch commit 2</div>
         <div>developer/login branch commit 1</div>
         <div>developer/login branch commit 2</div>
+        <div>Main branch commit 4</div>
+        <p>11</p>
+        <p>22</p>
+        <p>33</p>
       </div>
   );
 }
